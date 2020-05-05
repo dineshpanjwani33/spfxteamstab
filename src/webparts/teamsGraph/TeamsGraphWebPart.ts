f7172ddfd0e42a9172cc57d274238d778c6b733f
Dinesh Panjwani <dineshpanjwani33@gmail.com>
2020-05-05 10:16:53 +0530

    changes in ui

--- src/webparts/teamsGraph/TeamsGraphWebPart.ts
+++ src/webparts/teamsGraph/TeamsGraphWebPart.ts
@@ -32,8 +32,11 @@ private _renderList(messages:any):void{
 
  let html:string = '';
 
- html+= '<br/><textarea id="msg" name="msg" rows="7" cols="80"></textarea>';
+ html+= `<br/><textarea id="msg" name="msg" rows="6" cols="80" placeholder="Enter message" class="${styles.textareas}"></textarea>`;
  html+= '<br/><input type="file" name="file" id="myfileinput"  multiple>';
+ html+=`<ul class="" style='list-style-type:none;' ><li class="${styles.listItem}" >`;
+ html+= '<br/><span class="ms-font-1"><input type="checkbox" name="all" id="all">Select All Teams </input></span>';
+ html+= `</li></ul>`;
   messages.value.map((item: any)=>{
           html += `
           <ul class="${styles.list}" >
@@ -42,11 +45,10 @@ private _renderList(messages:any):void{
             </li>
           </ul>
           `;
-
           myhash[item.displayName] = item.id;         
 
         });
-        html+= '<center><input type="button" id="btn" value="Send"> </center><br/>';
+        html+= `<center><input type="button" id="btn" value="Send" class="${styles.button}"> </center><br/>`;
         let htmlElement: Element = this.domElement.querySelector('#msteamcontainer');
         htmlElement.innerHTML = html;
 
@@ -65,6 +67,15 @@ private _setButtonEventHandlers():void{
         htmlElement.innerHTML = "<span style='color:red;'>Message successfully sent</span>";
       });
   })
+
+  this.domElement.querySelector('#all').addEventListener('click',()=>{
+    var element = <HTMLInputElement> this.domElement.querySelector('#all');
+    var isChecked = element.checked?true:false;
+    var checkboxes : NodeListOf<Element> = this.domElement.querySelectorAll('input[type="checkbox"]');
+    for (var checkbox of checkboxes as any) {
+      checkbox.checked = isChecked;
+  }
+  });
 }
 
 private async _formsubmission():Promise<any>{
@@ -72,6 +83,8 @@ private async _formsubmission():Promise<any>{
   htmlElement.innerHTML = "<span style='color:red;'>Please wait while message is sending</span>";
   var message:HTMLTextAreaElement = this.domElement.querySelector('#msg');
   var checkboxes: NodeListOf<Element> = this.domElement.querySelectorAll('input[type="checkbox"]:checked');
+
+  console.log("Checkbox: ",checkboxes.item(0));
 
   var fileInput:HTMLInputElement = this.domElement.querySelector("#myfileinput") ;
 
@@ -85,165 +98,153 @@ private async _formsubmission():Promise<any>{
   };
 
   var file;
-
   let channelId: string[] = [];
   var promise2 = null;
   var fileContext = "";
  var fnames = []; 
    for ( var checkbox of checkboxes as any) {
-    
-    console.log("Team Name: "+checkbox.name+" : "+myhash[checkbox.name]);
-    //var teamId = myhash[checkbox.name];
-    var apiCall = "/teams/"+myhash[checkbox.name]+"/channels";
-    await new Promise(resolve => {
-      this.context.msGraphClientFactory
-      .getClient()
-      .then((client:MSGraphClient):void=>{
-          client
-          .api(apiCall)
-          .version("beta")
-          .get((error, messages: any, rawResponse?: any) => {
-            console.log("Team Id: "+myhash[checkbox.name]);
-            console.log(" Channel Id: "+messages.value[0].id + "Message: "+message.value);
-            //channelId.push(messages.value[0].id);
-            var msgApi = apiCall+"/"+messages.value[0].id+"/messages";
-            
-              if(files.length>0){
+     if(checkbox.name!=='all'){
+      console.log("Team Name: "+checkbox.name+" : "+myhash[checkbox.name]);
+      //var teamId = myhash[checkbox.name];
+      var apiCall = "/teams/"+myhash[checkbox.name]+"/channels";
+      await new Promise(resolve => {
+        this.context.msGraphClientFactory
+        .getClient()
+        .then((client:MSGraphClient):void=>{
+            client
+            .api(apiCall)
+            .version("beta")
+            .get((error, messages: any, rawResponse?: any) => {
+              console.log("Team Id: "+myhash[checkbox.name]);
+              console.log(" Channel Id: "+messages.value[0].id + "Message: "+message.value);
+              //channelId.push(messages.value[0].id);
+              var msgApi = apiCall+"/"+messages.value[0].id+"/messages";
+              
+                if(files.length>0){
 
-             //upload files
-             var siteApi = "/sites/root:/sites/"+checkbox.name;
+              //upload files
+              var siteApi = "/sites/root:/sites/"+checkbox.name;
 
-             var clientSPContext:string = "";
+              var clientSPContext:string = "";
 
-            this.context.msGraphClientFactory
-             .getClient()
-             .then((client:MSGraphClient):void=>{
-                 client
-                 .api(siteApi)
-                 .version("beta")
-                 .get((error, messages: any, rawResponse?: any) => {
+              this.context.msGraphClientFactory
+              .getClient()
+              .then((client:MSGraphClient):void=>{
+                  client
+                  .api(siteApi)
+                  .version("beta")
+                  .get((error, messages: any, rawResponse?: any) => {
 
-                  var siteId:string = messages.id;
+                    var siteId:string = messages.id;
 
-                  clientSPContext = messages.webUrl;
+                    clientSPContext = messages.webUrl;
 
-                  console.log("Site Id: ",siteId);
-                  var driveApi = "/sites/"+siteId+"/drives";
-                  this.context.msGraphClientFactory
-                  .getClient()
-                  .then((client:MSGraphClient):void=>{
-                      client
-                      .api(driveApi)
-                      .version("beta")
-                      .get((error, messages: any, rawResponse?: any) => {
-                          var driveId:string = messages.value[0].id;
-                          console.log("Drive Id: ",driveId);
+                    console.log("Site Id: ",siteId);
+                    var driveApi = "/sites/"+siteId+"/drives";
+                    this.context.msGraphClientFactory
+                    .getClient()
+                    .then((client:MSGraphClient):void=>{
+                        client
+                        .api(driveApi)
+                        .version("beta")
+                        .get((error, messages: any, rawResponse?: any) => {
+                            var driveId:string = messages.value[0].id;
+                            console.log("Drive Id: ",driveId);
+                            var fileUrl ="";
+                            var p1,p2,p3,p4;
+                            fnames=[];
+                            
+                            p1 = new Promise(resolvesOFor=>{ 
+                              for (var i = 0; i < files.length; i++) {
+                              p2 = new Promise(resolvesIFor=>{
+                                file = files[i];
+                                fnames.push(file.name);
+                                var uploadApi = driveApi + "/"+driveId+"/root:/General/"+file.name+":/content";
 
-                          // this._uploadFilesToSP(files,driveApi,driveId).then(resss=>{
-                          //   console.log("File urlsdd: ",resss)
-                          // });
-                          var fname;
-                          var fileUrl ="";
-                          var p1,p2,p3,p4;
-                          fnames=[];
-                          
-                          p1 = new Promise(resolvesssss=>{ 
-                            for (var i = 0; i < files.length; i++) {
-                            p2 = new Promise(resolves=>{
-                              file = files[i];
-                              fnames.push(file.name);
-                              fname=file.name;
-                              var uploadApi = driveApi + "/"+driveId+"/root:/General/"+file.name+":/content";
-
-                              p3=this._getFileRead(file).then(response=>{
-                             
-                              console.log("Result type: ",typeof( response));
-                              //console.log("Result msgs type: ",typeof( Messagess));
-                              console.log("msgs: ", response);
-                              //uploadMessage = uploadMessage.substring(1, uploadMessage.length() - 1)
+                                p3=this._getFileRead(file).then(response=>{
                               
-                              p4=this.context.msGraphClientFactory
-                              .getClient()
-                              .then((client:MSGraphClient):void=>{
-                                  client
-                                  .api(uploadApi)
-                                  .version("beta")
-                                  .put(response)
-                                  .then((res)=>{
-                                    fileUrl = fileUrl+" "+res.webUrl;
-                                    console.log("Weburl: ",fileUrl);
+                                console.log("Result type: ",typeof( response));
+                                //console.log("Result msgs type: ",typeof( Messagess));
+                                console.log("msgs: ", response);
+                                //uploadMessage = uploadMessage.substring(1, uploadMessage.length() - 1)
+                                
+                                p4=this.context.msGraphClientFactory
+                                .getClient()
+                                .then((client:MSGraphClient):void=>{
+                                    client
+                                    .api(uploadApi)
+                                    .version("beta")
+                                    .put(response)
+                                    .then((res)=>{
+                                      fileUrl = fileUrl+" "+res.webUrl;
+                                      console.log("Weburl: ",fileUrl);
+                                      
+                                    })
                                     
                                   })
-                                  
                                 })
-                              })
-                          resolves();
-                          
-                        })
-                         resolvesssss()
-                      }
-                      //resolvesssss(fileUrl)
-                    })
-                    Promise.all([p1,p2,p3,p4]).then((res)=>{
-                      console.log("urls: ",res)
-                      console.log("urlss: ",fileUrl)
-                      fileContext="";
-
-                      var chatMessage = {
-                        subject: null,
-                        body: {
-                            contentType: "html",
-                            content: message.value + fnames.map((ele)=>{
-                              var temp=clientSPContext +"/"+"Shared%20Documents/General/"+ele;
-                              return "<br/><a href='"+temp+"'>"+ele+"</a>";
-                            })
+                              resolvesIFor();                            
+                          })
+                          resolvesOFor()
                         }
-                      }
-                      this.context.msGraphClientFactory
-                        .getClient()
-                        .then((clients:MSGraphClient):void=>{
-                            clients
-                            .api(msgApi)
-                            .version("beta")
-                            .post(chatMessage)
-                        })
-                        resolve();
-
-                    })                          
+                        //resolvesssss(fileUrl)
                       })
-                    })
-                 })
-                }); 
-            }
-            else{
-            
-            var chatMessage = {
-              subject: null,
-              body: {
-                  contentType: "html",
-                  content: message.value 
+                      Promise.all([p1,p2,p3,p4]).then((res)=>{
+                        console.log("urls: ",res)
+                        console.log("urlss: ",fileUrl)
+                        fileContext="";
+
+                        var chatMessage = {
+                          subject: null,
+                          body: {
+                              contentType: "html",
+                              content: message.value + fnames.map((ele)=>{
+                                var temp=clientSPContext +"/"+"Shared%20Documents/General/"+ele;
+                                return "<br/><a href='"+temp+"'>"+ele+"</a>";
+                              })
+                          }
+                        }
+                        this.context.msGraphClientFactory
+                          .getClient()
+                          .then((clients:MSGraphClient):void=>{
+                              clients
+                              .api(msgApi)
+                              .version("beta")
+                              .post(chatMessage)
+                          })
+                          resolve();
+
+                      })                          
+                        })
+                      })
+                  })
+                  }); 
               }
-            }
-            this.context.msGraphClientFactory
-              .getClient()
-              .then((clients:MSGraphClient):void=>{
-                  clients
-                  .api(msgApi)
-                  .version("beta")
-                  .post(chatMessage)
-              })
-            resolve();
-            }
+              else{
+              
+              var chatMessage = {
+                subject: null,
+                body: {
+                    contentType: "html",
+                    content: message.value 
+                }
+              }
+              this.context.msGraphClientFactory
+                .getClient()
+                .then((clients:MSGraphClient):void=>{
+                    clients
+                    .api(msgApi)
+                    .version("beta")
+                    .post(chatMessage)
+                })
+              resolve();
+              }
+            })            
+          })    
         })
-            
-    })
-    
-    })
+    }
   }
-
 }
-
-
 
   private  _getFileRead(file):Promise<any>{ 
      return new Promise<any>((resolve,reject) =>{
@@ -282,12 +283,10 @@ private async _formsubmission():Promise<any>{
       .api("/me/joinedTeams")
       .version("beta")
       .get((error, messages: any, rawResponse?: any) => {
-       
-        
         if(error){
           console.log("Error: "+error);
         }
-          this._renderList(messages);
+        this._renderList(messages);
         
       });
     });
